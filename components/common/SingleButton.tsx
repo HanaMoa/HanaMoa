931e030f59a5a7980e9e355225a181fc3f201e72
ujii <wcm9763@naver.com>
2026-01-20 10:36:31 +0900

    feat: 기본 버튼 ui

---
+++ components/common/SingleButton.tsx
@@ -0,0 +1,21 @@
+import { Button } from '../ui/button';
+
+type Props = {
+  label: string; // 버튼에 들어갈 텍스트
+  formAction?: (formData: FormData) => Promise<void>;
+  redirectTo?: string;
+};
+
+export function SingleButton({ label, formAction, redirectTo }: Props) {
+  return (
+    <form action={formAction}>
+      <input type="hidden" name="redirectTo" value={redirectTo} />
+      <Button
+        type="submit"
+        className="h-[49px] w-[285px] rounded-[10px] bg-[#00A998] text-[#F6F7F9] text-[16px] hover:bg-[#017F70]"
+      >
+        {label}
+      </Button>
+    </form>
+  );
+}
