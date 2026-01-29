399755b83bb58695bf09e6c9886b65aa954b4de7
ujii <wcm9763@naver.com>
2026-01-29 09:47:46 +0900

    Revert "fix: 이미지 업로드 key 값에 type 추가"
    This reverts commit 3ec1364d0a45948c7d8640bddee82cf1a0d5c03d.

--- components/info/weddingInfo/PhotoUpload.tsx
+++ components/info/weddingInfo/PhotoUpload.tsx
@@ -161,7 +161,6 @@ export function PhotoUpload({
       const keys = await upload(
         selected.map((s) => s.file),
         eventId,
-        'gallery',
       );
 
       // keys를 방금 추가한 items 순서대로 매핑
