4bda292845c5b93c9363e94bf7492a154746bd78
ujii <wcm9763@naver.com>
2026-02-02 15:12:46 +0900

    fix/ merge 에러 해결

--- app/message/manual/page.tsx
+++ app/message/manual/page.tsx
@@ -1,19 +1,11 @@
 import { Suspense } from 'react';
 import ManualClient from './ManualClient';
 
-export default function MessageManualPage() {
-  return (
-    <Suspense fallback={<div className="min-h-dvh w-full bg-[#F6F7F9]" />}>
-      <ManualClient />
-    </Suspense>
-  );
-}
-
 // 메인 페이지 컴포넌트
 export default function MessageManualPage() {
   return (
     <Suspense fallback={<div className="min-h-dvh w-full bg-[#F6F7F9]" />}>
-      <ManualContent />
+      <ManualClient />
     </Suspense>
   );
 }
